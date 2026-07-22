13120538ee61909a844322110a9b65d886445b76
Harsh <harshkumar.22167@gmail.com>
2026-07-22 06:13:10 +0530

    Add files via upload

---
+++ prog1_1my_introduction.java
@@ -0,0 +1,8 @@
+class prog1_1my_introduction{
+public static void main(String[] args){
+System.out.println("Myself Dharmit Dhruve");
+System.out.println("I am from India");
+System.out.println("Right now I am pursuing BCA from Marwadi University, Rajkot");
+System.out.println("Thank you");
+}
+}
